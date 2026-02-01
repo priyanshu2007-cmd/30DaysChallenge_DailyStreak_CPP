feec93a933d405f533b2c4d8495ee28812d8c669
Priyanshu Yadav <priyanshu@Priyanshus-MacBook-Air.local>
2026-02-02 01:45:39 +0530

    1feb

---
+++ WelcomeTextonSeparateLine1feb.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome!" << endl;
+    cout << "We're glad you're here." << endl;
+    cout << "Let's get started." << endl;
+    return 0;
+}
---
+++ size1feb.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"Size of Fundamental Data Types in C++\n\n";
+cout<<"Size of char:"<<sizeof(char)<<" byte(s)"<<endl;
+cout<<"Size of int:"<<sizeof(int)<<" byte(s)"<<endl;
+cout<<"Size of float:"<<sizeof(float)<<" byte(s)"<<endl;
+cout<<"Size of double:"<<sizeof(double)<<" byte(s)"<<endl;
+cout<<"Size of short int:"<<sizeof(short int)<<" byte(s)"<<endl;
+cout<<"Size of long int:"<<sizeof(long int)<<" byte(s)"<<endl;
+cout<<"Size of long long int:"<<sizeof(long long int)<<" byte(s)"<<endl;
+cout<<"Size of bool:"<<sizeof(bool)<<" byte(s)"<<endl;
+return 0;
+}
