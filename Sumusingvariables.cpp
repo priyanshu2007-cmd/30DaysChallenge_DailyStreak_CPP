e1fcc5abff34320059678fa64482014af428bab1
Priyanshu Yadav <priyanshu@Priyanshus-MacBook-Air.local>
2026-02-04 01:19:10 +0530

    2feb

---
+++ Sumusingvariables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    int total;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    total = num1 + num2;
+    cout << "The sum is: " << total << endl;
+    return 0;
+}
