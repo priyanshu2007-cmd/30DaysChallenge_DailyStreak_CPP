d98e717c1713237e01816dbeb683b235c1bc3957
Priyanshu Yadav <priyanshu@Priyanshus-MacBook-Air.local>
2026-02-07 00:26:39 +0530

    7 feb

---
+++ LimitsofInteger.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Limits of Integer Data Types\n\n";
+    cout << "short int : -32768 to 32767" << endl;
+    cout << "int : -2147483648 to 2147483647" << endl;
+    cout << "long int : -2147483648 to 2147483647" << endl;
+    cout << "long long int : -9223372036854775808 to 9223372036854775807" << endl;
+    cout << "\nUnsigned types:\n";
+    cout << "unsigned short : 0 to 65535" << endl;
+    cout << "unsigned int : 0 to 4294967295" << endl;
+    cout << "unsigned long long : 0 to 18446744073709551615" << endl;
+    return 0;
+}
