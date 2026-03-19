e931cf9b01dc98e15ad06a70d8fdd740904f5a1b
Priyanshu Yadav <priyanshu@Priyanshus-MacBook-Air.local>
2026-03-20 02:09:20 +0530

    19th march

---
+++ duplicate.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[100], n,i,j,k;
+    cout<<"Enter size of array";
+    cin>>n;
+    cout<<"Enter elements";
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(a[i]==a[j]){
+                for(k=j;k<n-1;k++){
+                    a[k]=a[k+1];
+                }
+                n--;
+                j--;
+            }
+        }
+    }
+    cout<<"Unique elements";
+    for(i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
